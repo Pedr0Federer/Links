f0996294d6bcfff2fd0180fea0718d8abf26b110
Pedr0Federer <Pedr0Federer@users.noreply.github.com>
2026-07-30 00:56:37 +0300

    feat: add workflow dispatch self-chaining for 5-minute Kicks updates

--- scripts/update-kicks.js
+++ scripts/update-kicks.js
@@ -39,7 +39,7 @@ function toEntries(rawList) {
 async function fetchKicksLeaderboard() {
     const browser = await puppeteer.launch({
         headless: "new",
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
     });
 
     let json;
